63f32cfbb96db3dde8c49d89e7f3259f1f81be2e
v-juann <vjuann@gmail.com>
2023-11-27 15:52:18 -0300

    Edições na inclusão, localização e listagem de contatos.

--- trabalho.c
+++ trabalho.c
@@ -1,5 +1,4 @@
-/*
-Universidade do Estado de Santa Catarina
+/* Universidade do Estado de Santa Catarina
 Alunos: Nícolas Vinícius Lobo Morais, Vinicius Juann Pereira
 Trabalho Final - Agenda de Contatos
 O objetivo deste trabalho consiste em desenvolver um programa para manter as informações de uma agenda de contatos utilizando variáveis compostas. 
@@ -15,25 +14,28 @@ Uma sugestão é incrementar uma unidade no último identificador válido de um 
 As opções de excluir, alterar e localizar um contato deve realizada a partir de um identificador do contato informado pelo usuário. 
 A avaliação do trabalho deve levar em conta o atendimento a todos os requisitos funcionais, bem como a organização e uso de boas práticas de desenvolvimento no código.*/
 
+//Cabeçalho
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<malloc.h>
 
 //Estrutura do contato
 typedef struct{
     int codigo;
-    char nome[50];
-    int telefone;
-    char tipo[15];
+    char nome[15];
+    char telefone[12];
+    //char tipo[15];
 }Contato;
 
 //Início do programa
 int main(){
 
 //Declaração de variáveis
-    int opcao, n = 0;
-    Contato *contato = NULL;
-
+    int opcao, ident, n = 0;
+    //Atribui um endereço não nulo ao ponteiro
+    Contato *contato = (Contato *) calloc(1, sizeof(Contato));
+    
 //Estrutura de repetição que mantém o programa rodando até o usuário optar por sair
     do{
         printf("Selecione a opção: \n");
@@ -46,21 +48,20 @@ int main(){
         scanf("%d", &opcao);
 
         switch(opcao){
+            
             case 1:
-                    contato = (Contato *)realloc(contato, n* sizeof(Contato));
-                    printf("Digite o código do contato: \n");
-                    scanf("%d", contato[n].codigo);
+            //Inclusão de novo contato                    
+                    contato = realloc (contato, (sizeof(Contato)*(n+1)));
+                    (contato+n)->codigo = n+1;
 
-                    printf("Digite o nome do contato: \n");
+                    printf("Digite o nome do contato que será salvo com o código %d: \n",contato[n].codigo);
                     scanf("%s", contato[n].nome);
 
-                    printf("Digite o número de telefone do contato: \n");
-                    scanf("%d", contato[n].telefone);
-
-                    printf("Selecione o tipo de número (pessoal ou trabalho): \n");
-                    scanf("%s", contato[n].tipo);
-                n++;
+                    printf("Digite o número de telefone do contato que será salvo com o código %d: \n",contato[n].codigo);
+                    scanf("%s", contato[n].telefone);
+                    n++;
             break;
+
             case 2:
                 if (n > 0) {
                     printf("Contatos existentes:\n");
@@ -94,18 +95,53 @@ int main(){
                 } else {
                     printf("Nenhum contato cadastrado para excluir.\n");
                 }
-                break;
+            break;
+
             case 3:
                 //Alterar um contato existente
+                printf("Digite o código do contato que você quer alterar: \n");
+                scanf("%d", &ident);
+
+                if(ident > 0 && ident <= n){                    
+                    printf("Digite o novo valor para o o nome do contato %s (%s): \n",contato[ident-1].codigo, contato[ident-1].nome);
+                    scanf("%s", contato[ident].nome);
+                    printf("Digite o novo valor para o telefone do contato %s (%s): \n",contato[ident-1].codigo, contato[ident-1].telefone);
+                    scanf("%s", contato[ident].telefone);
+                }
+                else{
+                    printf("Contato inválido!\n");
+                }
+            break;
+            
             case 4:
-                // Listar todos os contatos cadastrados
+            //Lista todos os contatos cadastrados
+                for(int w = 0; w < n; w++){
+                    printf("\n-- Contato %d --\n", w+1);
+                    printf("Código: %d \n", contato[w].codigo);
+                    printf("Nome: %s \n", contato[w].nome);
+                    printf("Telefone: %s \n", contato[w].telefone);
+                }
             break;
+
             case 5:
-                //Localizar um contato
+            //Localizar um contato
+                printf("Digite o código do contato que você quer localizar: \n");
+                scanf("%d", &ident);
+
+                if(ident > 0 && ident <= n){
+                    printf("\n-- Contato %d --\n", ident);
+                    printf("Código: %d \n", contato[ident-1].codigo);
+                    printf("Nome: %s \n", contato[ident-1].nome);
+                    printf("Telefone: %s \n", contato[ident-1].telefone);
+                }
+                else{
+                    printf("Contato inválido!\n");
+                }
             break;
+
+            //Opção padrão para encerrar o programa quando o usuário apertar uma tecla que não seja de 1 a 5
             default:
-                printf("Opção inválida. Por favor, insira um número entre 1 e 5.\n");
-            break;
+                return 0;
         }
-    }while (opcao >= 1 && opcao <= 5);;
+    }while (main!=0);
 }
